ca68806a82a074c0b67c04d62a2408c582efc502
DarylMartinDipp <didieravangers@gmail.com>
2021-05-28 12:29:51 +0200

    Image

--- Projet IHM/Projet IHM/FormMenu.Designer.vb
+++ Projet IHM/Projet IHM/FormMenu.Designer.vb
@@ -28,48 +28,55 @@ Partial Class FormMenu
         Me.BtnScores = New System.Windows.Forms.Button()
         Me.BtnQuitter = New System.Windows.Forms.Button()
         Me.LblNom = New System.Windows.Forms.Label()
+        Me.PictureBoxMemory = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBoxMemory, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ComboBoxNom
         '
         Me.ComboBoxNom.FormattingEnabled = True
-        Me.ComboBoxNom.Location = New System.Drawing.Point(158, 118)
+        Me.ComboBoxNom.Location = New System.Drawing.Point(237, 182)
+        Me.ComboBoxNom.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.ComboBoxNom.Name = "ComboBoxNom"
-        Me.ComboBoxNom.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxNom.Size = New System.Drawing.Size(180, 28)
         Me.ComboBoxNom.TabIndex = 0
         '
         'BtnJouer
         '
-        Me.BtnJouer.Location = New System.Drawing.Point(107, 164)
+        Me.BtnJouer.Location = New System.Drawing.Point(160, 252)
+        Me.BtnJouer.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BtnJouer.Name = "BtnJouer"
-        Me.BtnJouer.Size = New System.Drawing.Size(125, 23)
+        Me.BtnJouer.Size = New System.Drawing.Size(188, 35)
         Me.BtnJouer.TabIndex = 1
         Me.BtnJouer.Text = "Jouer"
         Me.BtnJouer.UseVisualStyleBackColor = True
         '
         'BtnOptions
         '
-        Me.BtnOptions.Location = New System.Drawing.Point(107, 194)
+        Me.BtnOptions.Location = New System.Drawing.Point(160, 298)
+        Me.BtnOptions.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BtnOptions.Name = "BtnOptions"
-        Me.BtnOptions.Size = New System.Drawing.Size(125, 23)
+        Me.BtnOptions.Size = New System.Drawing.Size(188, 35)
         Me.BtnOptions.TabIndex = 2
         Me.BtnOptions.Text = "Options"
         Me.BtnOptions.UseVisualStyleBackColor = True
         '
         'BtnScores
         '
-        Me.BtnScores.Location = New System.Drawing.Point(107, 223)
+        Me.BtnScores.Location = New System.Drawing.Point(160, 343)
+        Me.BtnScores.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BtnScores.Name = "BtnScores"
-        Me.BtnScores.Size = New System.Drawing.Size(125, 23)
+        Me.BtnScores.Size = New System.Drawing.Size(188, 35)
         Me.BtnScores.TabIndex = 3
         Me.BtnScores.Text = "Score"
         Me.BtnScores.UseVisualStyleBackColor = True
         '
         'BtnQuitter
         '
-        Me.BtnQuitter.Location = New System.Drawing.Point(107, 252)
+        Me.BtnQuitter.Location = New System.Drawing.Point(160, 388)
+        Me.BtnQuitter.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BtnQuitter.Name = "BtnQuitter"
-        Me.BtnQuitter.Size = New System.Drawing.Size(125, 23)
+        Me.BtnQuitter.Size = New System.Drawing.Size(188, 35)
         Me.BtnQuitter.TabIndex = 4
         Me.BtnQuitter.Text = "Quitter"
         Me.BtnQuitter.UseVisualStyleBackColor = True
@@ -77,26 +84,41 @@ Partial Class FormMenu
         'LblNom
         '
         Me.LblNom.AutoSize = True
-        Me.LblNom.Location = New System.Drawing.Point(55, 122)
+        Me.LblNom.Location = New System.Drawing.Point(82, 188)
+        Me.LblNom.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblNom.Name = "LblNom"
-        Me.LblNom.Size = New System.Drawing.Size(96, 13)
+        Me.LblNom.Size = New System.Drawing.Size(142, 20)
         Me.LblNom.TabIndex = 5
         Me.LblNom.Text = "Entrez votre nom : "
         '
+        'PictureBoxMemory
+        '
+        Me.PictureBoxMemory.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.PictureBoxMemory.Image = Global.Projet_IHM.My.Resources.Resources.memory
+        Me.PictureBoxMemory.ImageLocation = ""
+        Me.PictureBoxMemory.Location = New System.Drawing.Point(86, 32)
+        Me.PictureBoxMemory.Name = "PictureBoxMemory"
+        Me.PictureBoxMemory.Size = New System.Drawing.Size(358, 111)
+        Me.PictureBoxMemory.TabIndex = 6
+        Me.PictureBoxMemory.TabStop = False
+        '
         'FormMenu
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(351, 279)
+        Me.ClientSize = New System.Drawing.Size(526, 429)
+        Me.Controls.Add(Me.PictureBoxMemory)
         Me.Controls.Add(Me.LblNom)
         Me.Controls.Add(Me.BtnQuitter)
         Me.Controls.Add(Me.BtnScores)
         Me.Controls.Add(Me.BtnOptions)
         Me.Controls.Add(Me.BtnJouer)
         Me.Controls.Add(Me.ComboBoxNom)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "FormMenu"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Menu principal"
+        CType(Me.PictureBoxMemory, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -108,4 +130,5 @@ Partial Class FormMenu
     Friend WithEvents BtnScores As Button
     Friend WithEvents BtnQuitter As Button
     Friend WithEvents LblNom As Label
+    Friend WithEvents PictureBoxMemory As PictureBox
 End Class
--- Projet IHM/Projet IHM/My Project/Resources.Designer.vb
+++ Projet IHM/Projet IHM/My Project/Resources.Designer.vb
@@ -119,5 +119,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Recherche une ressource localisée de type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property memory() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("memory", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
